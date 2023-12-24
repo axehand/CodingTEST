c275d862b99a92e65d8ac63412db4f630fda76a8
godbucket <toa2ek3@naver.com>
2023-12-24 18:40:30 +0900

    윤년 문제 풀이

--- leapyear.py
+++ leapyear.py
@@ -5,3 +5,16 @@
 # 예를 들어, 2012년은 4의 배수이면서 100의 배수가 아니라서 윤년이다. 
 # 1900년은 100의 배수이고 400의 배수는 아니기 때문에 윤년이 아니다. 
 # 하지만, 2000년은 400의 배수이기 때문에 윤년이다.
+import sys
+
+year = int(sys.stdin.readline().strip())
+
+if int(year % 4) == 0: #4의 배수일때
+    if int(year % 100) != 0:# 100의 배수가 아니면 윤년
+        sys.stdout.write('1')
+    elif int(year % 400) == 0:# 400의 배수면 윤년
+        sys.stdout.write('1')
+    else:
+        sys.stdout.write('0')
+else:
+    sys.stdout.write('0')
