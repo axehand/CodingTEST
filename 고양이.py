ec40239aa6b3542b51fdad0bb0d3c0e1a95f90d9
godbucket <toa2ek3@naver.com>
2023-12-30 17:52:56 +0900

    filename

---
+++ 고양이.py
@@ -0,0 +1,11 @@
+#아래 예제와 같이 고양이를 출력하시오.
+import sys
+sys.stdout.write("\    /\\" + '\n')
+sys.stdout.write(" )  ( ')" + '\n')
+sys.stdout.write("(  /  )" + '\n')
+sys.stdout.write(" \(__)|" + '\n')
+
+print('\    /\\')
+print(' )  ( \')')
+print('(  /  )')
+print(' \(__)|')
